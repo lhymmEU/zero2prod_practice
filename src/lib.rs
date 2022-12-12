227448f9cf6042f0ada5ad6c7fe32c3ef1688ccf
lhymmA <42554118+lhymmA@users.noreply.github.com>
2022-12-12 16:35:05 +0100

    finish first migration of the local database

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,20 @@
-// To-Do list:
-//      1. what is async? : https://www.youtube.com/watch?v=skos4B5x7qE
-//      2. what is guard trait?
-//      3. what is Futures?: https://cfsamson.github.io/books-futures-explained/introduction.html
-//      4. what is procedural macro?
-//      5. what is builder-pattern?
-//      6. what is user-visible regression?
-
 use actix_web::{web, App, HttpServer, Responder, HttpResponse};
 use actix_web::dev::Server;
 use std::net::TcpListener;
 
 // a dummy function to test if the bare minimum of code works
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 // start the server and return a Tokio server handler,
@@ -26,6 +28,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
         // this app block handles the application layer logic
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,19 @@
 use std::net::TcpListener;
 
+// this function handles the logic of spawn a server to the background
+fn spawn_app() -> String {
+    // start a tokio tcp listener on OS port 0 to get a random port number
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port.");
+    // extract the port number from the TCP socket server
+    let port = listener.local_addr().unwrap().port();
+    // get a server handler from the library
+    let server = zero2prod::run(listener).expect("Failed to bind address.");
+    // spawn the server
+    let _ = tokio::spawn(server);
+    // return the actual address the server is running on
+    format!("http://127.0.0.1:{}", port)
+}
 
 #[tokio::test]
 async fn health_check_works() {
@@ -21,17 +35,47 @@ async fn health_check_works() {
 
 }
 
-// this function handles the logic of spawn a server to the background
-fn spawn_app() -> String {
-    // start a tokio tcp listener on OS port 0 to get a random port number
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port.");
-    // extract the port number from the TCP socket server
-    let port = listener.local_addr().unwrap().port();
-    // get a server handler from the library
-    let server = zero2prod::run(listener).expect("Failed to bind address.");
-    // spawn the server
-    let _ = tokio::spawn(server);
-    // return the actual address the server is running on
-    format!("http://127.0.0.1:{}", port)
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email")
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
