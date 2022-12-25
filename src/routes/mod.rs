9c541c9789c9801447a1d71821ef2c71269e515d
lhymmEU <italy.18429@gmail.com>
2022-12-25 03:21:50 +0100

    add send newsletter handler

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,7 +2,9 @@
 mod health_check;
 mod subscriptions;
 mod subscriptions_confirm;
+mod newsletters;
 // re-export useful functions
 pub use health_check::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
+pub use newsletters::*;
--- src/startup.rs
+++ src/startup.rs
@@ -1,13 +1,12 @@
 use actix_web::{web, App, HttpServer};
 use actix_web::dev::Server;
 use std::net::TcpListener;
-use crate::routes::{ health_check, subscribe };
+use crate::routes::{ health_check, subscribe, confirm, publish_newsletter };
 use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
 use crate::email_client::EmailClient;
 use crate::configuration::{Settings, DatabaseSettings};
 use sqlx::postgres::PgPoolOptions;
-use crate::routes::confirm;
 
 pub struct Application {
     port: u16,
@@ -100,6 +99,7 @@ pub fn run(
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
+            .route("/newsletters", web::post().to(publish_newsletter))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
